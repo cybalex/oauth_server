73692c2a6acaa8d49f5524dbe381d0402367112b
Oleksii Zymovets <cybalex87@gmail.com>
2019-08-20 01:48:18 +0300

    doctrine installation, remove flex

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    #FOS\OAuthServerBundle\FOSOAuthServerBundle::class => ['all' => true],
 ];
